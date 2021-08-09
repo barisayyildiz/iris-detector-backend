5a9a1fa9b302b3acc3338e2fda8b660305d00242
barisayyildiz <barisayyildiz.1799@gmail.com>
2021-08-09 17:42:14 +0300

    api response düzenlendi

--- api/util.py
+++ api/util.py
@@ -8,8 +8,11 @@ def predict(obj):
 	petal_length = obj["petal_length"]
 
 	target_names = ['Setosa', 'Versicolor', 'Virginica']
-
-	print(os.listdir())
+	urls = [
+		"https://upload.wikimedia.org/wikipedia/commons/thumb/5/56/Kosaciec_szczecinkowaty_Iris_setosa.jpg/330px-Kosaciec_szczecinkowaty_Iris_setosa.jpg",
+		"https://upload.wikimedia.org/wikipedia/commons/2/27/Blue_Flag%2C_Ottawa.jpg",
+		"https://upload.wikimedia.org/wikipedia/commons/thumb/f/f8/Iris_virginica_2.jpg/1024px-Iris_virginica_2.jpg"
+	]
 
 	filename = 'iris_model'
 	infile = open(filename, 'rb')
@@ -17,5 +20,13 @@ def predict(obj):
 
 	infile.close()
 
-	return target_names[int(model.predict([[sepal_width, sepal_length, petal_width, petal_length]]))]
+	print(f'num : {model.predict([[sepal_length, sepal_width, petal_length, petal_width]])}')
+
+	index = int(model.predict([[sepal_length, sepal_width, petal_length, petal_width]]))
+	name = target_names[index]
+
+	return {
+		"name":name,
+		"url":urls[index]
+	}
 
